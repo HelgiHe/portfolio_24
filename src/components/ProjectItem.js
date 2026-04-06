baee6cf41c334fbc2401f45b2910768982512ab2
Helgi Helgason <helgihelgason@Helgis-Mac-mini.local>
2026-04-06 18:43:08 +0000

    animate project image with scroll

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,13 +2,24 @@
 import Image from "next/image";
 import Markdown from "react-markdown";
 import cn from "classnames";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProjectItem = ({ img, title, link, desc, className = "" }) => {
+  const imageRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: imageRef,
+    offset: ["start 90%", "end 35%"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [160, -160]);
+
   return (
     <div
       className={cn(
         "flex flex-col-reverse justify-between w-full pb-32",
-        className
+        className,
       )}
     >
       <div className="flex flex-col w-full lg:w-7/12">
@@ -25,14 +36,21 @@ export const ProjectItem = ({ img, title, link, desc, className = "" }) => {
         )}
         <Markdown className="text-lg font-normal">{desc}</Markdown>
       </div>
+
       <div className="flex justify-center w-full lg:w-5/12">
-        <Image
-          className="w-64 mt-16 border-8 rounded-3xl border-zinc-900 lg:w-80"
-          src={img}
-          width={350}
-          height={350}
-          alt={`${title} image`}
-        />
+        <motion.div
+          ref={imageRef}
+          style={{ y }}
+          className="will-change-transform"
+        >
+          <Image
+            className="w-64 mt-16 border-8 rounded-3xl border-zinc-900 lg:w-80"
+            src={img}
+            width={350}
+            height={350}
+            alt={`${title} image`}
+          />
+        </motion.div>
       </div>
     </div>
   );
